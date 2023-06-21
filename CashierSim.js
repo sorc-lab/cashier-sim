ce35eb041f3704177e7357bd28b875e4ad010ea8
macfisher <macfisher01@gmail.com>
2023-06-21 04:12:04 -0600

    item db w/ basic access

--- CashierSim.js
+++ CashierSim.js
@@ -1,25 +1,29 @@
-class CashierSim {
-    MOCK_GROCERY_ITEMS = [
-        {
-            "name": "item1",
-            "price": "$1.00"
-        },
-        {
-            "name": "item2",
-            "price": "$1.50"
-        },
-        {
-            "name": "item3",
-            "price": "$56.99"
-        }
-    ]
+const GroceryItem = {
+    name: null,
+    price: null
+}
 
-    constructor() {
+class GroceryItemDAO {
+    constructor(){}
 
+    seedDB() {
+        window.localStorage.setItem("mock_grocery_items", JSON.stringify(this.getMockGroceryItems()));
     }
 
-    seedGroceryItemsDb() {
-        window.localStorage.setItem("mock_grocery_items", JSON.stringify(this.MOCK_GROCERY_ITEMS));
+    getMockGroceryItems() {
+        const item1 = Object.create(GroceryItem);
+        item1.name = "item1";
+        item1.price = "$1.00";
+
+        const item2 = Object.create(GroceryItem);
+        item2.name = "item2";
+        item2.price = "$1.49";
+
+        const item3 = Object.create(GroceryItem);
+        item3.name = "item3";
+        item3.price = "$56.99";
+
+        return [item1, item2, item3];
     }
 
     getGroceryItems() {
@@ -29,9 +33,8 @@ class CashierSim {
 
 class Main {
     constructor() {
-        const cashierSim = new CashierSim();
-        cashierSim.seedGroceryItemsDb();
-        console.log(cashierSim.getGroceryItems());
+        const groceryItemDAO = new GroceryItemDAO();
+        groceryItemDAO.seedDB();
     }
 }
 
@@ -39,19 +42,19 @@ class Main {
 new Main();
 
 /* MVP (no graphics. bare-bones HTML. no scores or manager module. only pass/fail states applied.)
-    1. UI loads items.json into local storage. (use some mock items for MVP.)
-    2. NPCs are generated w/ random spending behavior and list of items to purchase. (use 1 bahavior for now)
-    3. NOTE: No server backend, no requests. We need state and a gameloop.
-    4. Player state is set to PLAYER_REQUEST_PAYMENT
-    5. Player is presented first NPC in queue's item list with prices.
-    6. Player is prompted to request a payment amount.
-    7. Player submits their payment request and enters into PLAYER_AWAITING_PAYMENT state.
-    8. NPC generates payment based on behavior trees. Player receives payment object, which is simply a wallet obj?
-    9. Player receives cash obj /w denoms. and enters PLAYER_RECEIVED_PAYMENT state.S
-    10. Player cash register is populated with customer payment.
-    11. Player is now prompted to return correct change to customer.
-    12. Player is presented with sliders and input fields that represent a cash obj.
-    13. Player fills out cash obj and hits submit.
-    14. Player now enters PLAYER_CHANGE_SUBMITTED state.
-    15. Finally, for MVP, check against a correct value and pass/fail then end the game loop.
+    [x] UI loads items.json into local storage. (use some mock items for MVP.)
+    [ ] NPCs are generated w/ random spending behavior and list of items to purchase. (use 1 bahavior for now)
+    [ ] NOTE: No server backend, no requests. We need state and a gameloop.
+    [ ] Player state is set to PLAYER_REQUEST_PAYMENT
+    [ ] Player is presented first NPC in queue's item list with prices.
+    [ ] Player is prompted to request a payment amount.
+    [ ] Player submits their payment request and enters into PLAYER_AWAITING_PAYMENT state.
+    [ ] NPC generates payment based on behavior trees. Player receives payment object, which is simply a wallet obj?
+    [ ] Player receives cash obj /w denoms. and enters PLAYER_RECEIVED_PAYMENT state.S
+    [ ] Player cash register is populated with customer payment.
+    [ ] Player is now prompted to return correct change to customer.
+    [ ] Player is presented with sliders and input fields that represent a cash obj.
+    [ ] Player fills out cash obj and hits submit.
+    [ ] Player now enters PLAYER_CHANGE_SUBMITTED state.
+    [ ] Finally, for MVP, check against a correct value and pass/fail then end the game loop.
 */
